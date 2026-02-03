a216a2f086f8d1beac805241113cf7394f503fbf
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-03 07:41:35 +0200

    add sof2gt_qmm repo to build init scripts

--- .github/build/windows/init.bat
+++ .github/build/windows/init.bat
@@ -1,2 +1,3 @@
 git clone https://github.com/thecybermind/qmm2.git ../qmm2
 git clone https://github.com/thecybermind/qmm_sdks.git ../qmm_sdks
+git clone https://github.com/thecybermind/sof2gt_qmm.git ../sof2gt_qmm
